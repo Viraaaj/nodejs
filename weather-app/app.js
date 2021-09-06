c9dbb4c044d1c36f18193d5820504a4a24113e28
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-09-06 20:56:16 +0530

    Customizing HTTP requests, weather app, mapbox, geocode

--- weather-app/app.js
+++ weather-app/app.js
@@ -9,9 +9,21 @@ request({ url: url, json: true }, (error, response) => {
   //   console.log("Weather api data:", data);
   //   console.log("Weather api data current:", data.current);
 
-  console.log("Weather api data current:", response.body.current);
+  //   console.log("Weather api data current:", response.body.current);
 
   console.log(
     `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`
+  );
+});
+
+//Getting longitude and latitude of location | mapbox
+
+const geocodeUrl =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Pune.json?access_token=pk.eyJ1IjoiYmF0bWFuMTI5OSIsImEiOiJja3Q4cnRqc2MxNTdnMm9wZWJ2d2lqdjVwIn0.Wwpp1gd7YlZc4n5VWV2-sg&limit=1";
+
+request({ url: geocodeUrl, json: true }, (error, response) => {
+  //   console.log("Mapbox response:", response.body);
+  console.log(
+    `Mapbox response longitude and latitude: ${response.body.features[0].center[0]}, ${response.body.features[0].center[1]}`
   );
 });
